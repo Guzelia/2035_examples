7c8d7ae413faf52bbce82b26e81f791dc4369cf4
fecitpotentiam <34218840+fecitpotentiam@users.noreply.github.com>
2020-11-19 01:09:34 +0500

    Update text_preparation.py

--- text_preparation.py
+++ text_preparation.py
@@ -13,11 +13,19 @@ with open('text.txt', 'r') as f:
 # Разделение текста на предложения
 tokenized_sentences = [sent for sent in sent_tokenize(text)]
 
+# Инициализация списка для нормализованных предложений
+normalized_sentences = []
+
 # Разделение слов в предложениях
 for sentence in tokenized_sentences:
     words = [word.lower() for word in word_tokenize(sentence) if word not in string.punctuation]
+    
+    # Инициализация списка для нормализованных слов
     normalized_words = []
 
     # Приведение к нормальной форме
     for word in words:
         normalized_words.append(morph.parse(word)[0].normal_form)
+    
+    # Добавление нормализованных слов в список нормализованных предложений
+    normalized_sentences.append(normalized_words)
